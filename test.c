77162b0a2edcd32b6f6d053bfa612d5768d44444
Ajai Nelson <22969541+Aurelius333@users.noreply.github.com>
2022-10-17 20:13:52 +0000

    Separate out png_to_array function

--- test.c
+++ test.c
@@ -21,22 +21,17 @@ void to_grayscale(unsigned long height, unsigned long width, unsigned char *colo
 	}
 }
 
-// also see https://gist.github.com/niw/5963798
-// That might be simpler
-
-int main() {
+void png_to_array(char* filename, unsigned char **array_ptr, unsigned long *height, unsigned long *width, int *channels) {
 	readpng_version_info();
 
 	FILE *fptr;
-	if ((fptr = fopen("L_00001.png", "r")) == NULL) {
+	if ((fptr = fopen(filename, "r")) == NULL) {
 		printf("Error! opening file\n");
 		exit(1);
 	}
 
-	unsigned long width = 0;
-	unsigned long height = 0;
-	int init_ret = readpng_init(fptr, &width, &height);
-	printf("readpng_init returned %d \t width=%lu height=%lu\n", init_ret, width, height);
+	int init_ret = readpng_init(fptr, width, height);
+	printf("readpng_init returned %d \t width=%lu height=%lu\n", init_ret, *width, *height);
 
 	unsigned char red = 0;
 	unsigned char green = 0;
@@ -44,17 +39,26 @@ int main() {
 	int bg_ret = readpng_get_bgcolor(&red, &green, &blue);
 	printf("readpng_get_bgcolor returned %d \t red=%u green=%u blue=%u\n", bg_ret, red, green, blue);
 
-	int channels = 0;
 	unsigned long rowbytes = 0;
-	unsigned char *image_data = readpng_get_image(&channels, &rowbytes);
-	printf("channels=%d rowbytes=%lu\n", channels, rowbytes);
+	*array_ptr = readpng_get_image(channels, &rowbytes);
+	printf("channels=%d rowbytes=%lu\n", *channels, rowbytes);
 
 	readpng_cleanup(false);
 
 	fclose(fptr);
 
-	printf("rowbytes should be width*channels = %lu*%d = %lu\n", width, channels, width*channels);
-	printf("size of image_data should be height*width*channels = %lu*%lu*%d = %lu\n", height, width, channels, height*width*channels);
+	printf("rowbytes should be width*channels = %lu*%d = %lu\n", *width, *channels, (*width)*(*channels));
+	printf("size of image_data should be height*width*channels = %lu*%lu*%d = %lu\n", *height, *width, *channels, (*height)*(*width)*(*channels));
+}
+
+// also see https://gist.github.com/niw/5963798
+// That might be simpler
+
+int main() {
+	unsigned long height, width;
+	int channels;
+	unsigned char *image_data;
+	png_to_array("L_00001.png", &image_data, &height, &width, &channels);
 
 	unsigned char *ptr = image_data;
 	for (unsigned long y = 0; y < height; y++) {
